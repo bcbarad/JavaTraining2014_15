a968817101fee4530d505409cc5429b3166f6cbc
chaitanya <chaitanya@A7080Q1D.corp.ads.valuelabs.net>
2014-11-20 16:33:05 +0530

    initial commit

---
+++ Chaitanya/project1/src/com/vl/training/sample/MySample.java
@@ -0,0 +1,9 @@
+package com.vl.training.sample;
+public final class MySample {
+    private MySample() {
+        System.out.println("this is a private constructor");
+    }
+    public static void main(final String[] args) {
+        System.out.println("Hello");
+    }
+}
