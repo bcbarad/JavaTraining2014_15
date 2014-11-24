30b918d79b65a816f20be70984f8ef11aae4293b
sowjanyabatchu <sowjanya.batchu@valuelabs.net>
2014-11-24 12:25:09 +0530

    updated

--- Sowjanya/project2/src/com/vl/training/sample/MySample.java
+++ Sowjanya/project2/src/com/vl/training/sample/MySample.java
@@ -2,25 +2,24 @@ package com.vl.training.sample;
 import java.util.Scanner;
 public class MySample {
     public static int countWords(String str) {
-	int count = 1 , j = 0;
-	   for(int i = 0 ; i <= str.length() - 1 ; i++) {
-		if ((str.charAt(i) == ' ')) {
-		    count++;
-		    j = i;
-		    //  System.out.println(count);
-			while ((str.charAt(j) == ' '))
-                            {
-				j++;
-              			}
-		    i = j - 1;
-		    }
-	    }
-	    return count;
-	}
+        int count = 1 , j = 0;
+        for (int i = 0; i <= str.length() - 1; i++) {
+            if ((str.charAt(i) == ' ')) {
+                count++;
+                j = i;
+                //  System.out.println(count);
+                while ((str.charAt(j) == ' ')) {
+                    j++;
+                }
+                i = j - 1;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
-	System.out.println("enter a sentence");
-	String sentence = in.nextLine();
-	System.out.print("Your sentence has " + countWords(sentence) + " words.");
-	}
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter a sentence");
+        String sentence = in.nextLine();
+        System.out.print("Your sentence has " + countWords(sentence) + " words.");
+    }
 }
