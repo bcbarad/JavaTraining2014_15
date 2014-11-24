7f768544f1ed3fd3c9b8a3021148f239eb30b2ef
Madhusudhan123 <madhusudhana.swarna@valuelabs.net>
2014-11-24 14:12:01 +0530

    Factorial series sum added

---
+++ Madhu/nFactsum/src/com/vl/training/sample/NFactSum.java
@@ -0,0 +1,20 @@
+package com.vl.training.sample;
+public final class NFactSum {
+    private NFactSum() {
+    }
+    static int  factorial(final int num) {
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+    public static void main(final String[] args) {
+        int sum = 0;
+        int number = Integer.parseInt(args[0]);
+        for (int i = 1; i < (number * 2); i *= 2) {
+            sum = sum + NFactSum.factorial(i);
+        }
+        System.out.println(" the sum of factorials series be: " + sum);
+    }
+}
