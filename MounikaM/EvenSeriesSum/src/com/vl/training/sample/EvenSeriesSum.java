0acadfba3708685859e2696345516d4a3f51dc0a
MounikaMylaram <mounika.mylaram@valuelabs.net>
2014-11-25 17:57:52 +0530

    Changes done to EvenseriesSum

--- MounikaM/EvenSeriesSum/src/com/vl/training/sample/EvenSeriesSum.java
+++ MounikaM/EvenSeriesSum/src/com/vl/training/sample/EvenSeriesSum.java
@@ -4,11 +4,15 @@ public class EvenSeriesSum {
     public static void main(final String[] args) {
         if (args.length == 1) {
             int range = Integer.parseInt(args[0]);
-            System.out.println("\n enter the range for sum is : " + range);
-            EvenSeriesSum es = new EvenSeriesSum();
-            int result = es.seriesSum(range);
+            if (range < 0) {
+                System.out.println("\n please enter posiitive numbers");
+            } else {
+                System.out.println("\n enter the range for sum is : " + range);
+                EvenSeriesSum es = new EvenSeriesSum();
+                int result = es.seriesSum(range);
 
-            System.out.println("\n Sum of the series upto given ranges is:" + result);
+                System.out.println("\n Sum of the series upto given ranges is:" + result);
+            }
         } else {
             System.out.println("\n please enter only one value");
         }
