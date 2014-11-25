de9a26849e0238df22e4731aaa42cdf5dfb9ed0a
sowjanyabatchu <sowjanya.batchu@valuelabs.net>
2014-11-25 10:27:23 +0530

    factorial

---
+++ Sowjanya/factorial/src/com/vl/training/sample/Factorial.java
@@ -0,0 +1,28 @@
+package com.vl.training.sample;
+import java.util.Scanner;
+public class Factorial {
+    public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //System.out.print("Enter the number ");
+        int n = scanner.nextInt();
+        int result = evennumbers(n);
+        System.out.println("The sum of factorial of" + n);
+        System.out.println(" even numbers is " + result);
+    }
+    public static int evennumbers(final int n) {
+        int sum = 1, j = 2;
+        for (int i = 1; i < n; i++) {
+            sum = sum + factorial(j);
+            j = j + 2;
+        }
+        return sum;
+    }
+    public static int factorial(final int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
+
