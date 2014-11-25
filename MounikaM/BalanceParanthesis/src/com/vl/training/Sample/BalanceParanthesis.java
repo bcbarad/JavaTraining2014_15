04ea4414e67303422a56201a9e2ef4d753a4e625
MounikaMylaram <mounika.mylaram@valuelabs.net>
2014-11-25 16:52:38 +0530

    canges are done to build.xml

--- MounikaM/BalanceParanthesis/src/com/vl/training/Sample/BalanceParanthesis.java
+++ MounikaM/BalanceParanthesis/src/com/vl/training/Sample/BalanceParanthesis.java
@@ -1,34 +1,35 @@
-/*package com.vl.training.Sample;*/
+package com.vl.training.Sample;
 
-public class BalanceParanthesis{
-   public static void main(String []args){
-     boolean d = BalanceParanthesis.balanceCheck(" ( This) ( ( py))");
-      if(d)
-       System.out.println("\n braces are balanced in given string");
-      else
-       System.out.println("\n braces are not balanced in given string:");
-  }
-
-   static boolean balanceCheck(String s){
-   int count = 0;
-    for(int index = 0; index < s.length() ; index++)
-    {
-      if(s.charAt(index) == '(' )
-        count += 1;
-      else if(s.charAt(index) == ')' )
-      {
-        if( count >= 0 )
-          count -= 1;
-        else
-          break; 
-      }
-      else
-        continue;
+public class BalanceParanthesis {
+    public static void main(final String []args) {
+        boolean d = BalanceParanthesis.balanceCheck(" ( This) ( ( py))");
+        if (d) {
+            System.out.println("\n braces are balanced in given string");
+        } else {
+            System.out.println("\n braces are not balanced in given string:");
+        }
     }
-    if( count == 0 )
-      return true;
-    else
-      return false;
-   }
+
+    static boolean balanceCheck(final String s) {
+        int count = 0;
+        for (int index = 0; index < s.length(); index++) {
+            if (s.charAt(index) == '(') {
+                count += 1;
+            } else if (s.charAt(index) == ')') {
+                if (count >= 0) {
+                    count -= 1;
+                }  else {
+                    break;
+                }
+            } else {
+                continue;
+            }
+        }
+        if (count == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
-   
+
