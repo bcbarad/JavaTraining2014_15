164367716748dd4a7c7bda2eb612919ae84fa2ee
Sravani Guddety <sravani.guddety@valuelabs.net>
2014-11-28 11:46:00 +0530

    added student but need changes

--- Sravani/student/src/marks/resolve/highest/StudentMarks.java
+++ Sravani/student/src/marks/resolve/highest/StudentMarks.java
@@ -2,7 +2,7 @@ package marks.resolve.highest;
 import java.util.Scanner;
 public class StudentMarks {
     Student[] students;
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Student s = new Student();
         Scanner sc = new Scanner(System.in);
         Total t = new Total();
@@ -15,7 +15,7 @@ public class StudentMarks {
         String highname = null;
         sm.students = new Student[nst];
         for (int i = 0; i < nst; i++) {
-            sm.students[i] = Student.readMe(sc,nst , nsb);
+            sm.students[i] = Student.readMe(sc , nst , nsb);
         }
         t.displayMax(nst , sm);
         t.displayIndividual(nst , nsb , sm);
@@ -25,13 +25,13 @@ class Student {
     String name;
     public Marks[] allMarks;
     int nsb , total;
-    public static Student readMe(Scanner sc , int nst , int nsb) {
+    public static Student readMe(Scanner sc , final int nst , final int nsb) {
         Student s = new Student();
         System.out.println("enter name of the student");
         s.name = sc.next();
         s.total = 0;
         s.allMarks = new Marks[nsb];
-        for (int i =0; i < nsb; i++) {
+        for (int i = 0; i < nsb; i++) {
             s.allMarks[i] = Marks.readMe(sc,nst);
             s.total += s.allMarks[i].submarks;
         }
@@ -42,7 +42,7 @@ class Student {
 class Marks {
     int submarks;
     String subname;
-    public static Marks readMe(Scanner sc , int nst) {
+    public static Marks readMe(Scanner sc , final int nst) {
         Marks m = new Marks();
         System.out.println("enter subname sb");
         m.subname = sc.next();
@@ -51,8 +51,8 @@ class Marks {
         return m;
     }
 }
-class Total{
-    public static void displayMax(int stucount , StudentMarks sm) {
+class Total {
+    public static void displayMax(final int stucount , StudentMarks sm) {
         int high = 0;
         String highname = "";
         for (int i = 0; i < stucount; i++) {
@@ -66,10 +66,10 @@ class Total{
     public static void displayIndividual(int stucount , int subjects , StudentMarks sm) {
         int high = 0;
         String highname = "" , sub = "";
-        for(int i = 0; i < subjects; i++) {
+        for (int i = 0; i < subjects; i++) {
             high = 0;
-            for(int j=0; j < stucount; j++) {
-                if(high < sm.students[i].allMarks[j].submarks) {
+            for (int j = 0; j < stucount; j++) {
+                if (high < sm.students[i].allMarks[j].submarks) {
                     high = sm.students[i].allMarks[j].submarks;
                     highname = sm.students[i].name;
                     sub = sm.students[i].allMarks[j].subname;
