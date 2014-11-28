d26bca0bc09a61fd073bbe431d21e3ec688be0b7
sowjanyabatchu <sowjanya.batchu@valuelabs.net>
2014-11-28 16:24:33 +0530

    changes done

--- Sowjanya/studentdata/src/com/vl/training/sample/StudentData.java
+++ Sowjanya/studentdata/src/com/vl/training/sample/StudentData.java
@@ -1,21 +1,24 @@
-package com.vl.training.sample;
+//package com.vl.training.sample;
 import java.util.Scanner;
-public final class StudentData {
+public class StudentData {
     private StudentData() {
     }
-    private Course[] studentdata = {};
+ public Course[] studentdata = {};
     public static void main(final String[] args) {
         int NUMBER_OF_STUDENTS = 0, NUMBER_OF_SUBJECTS = 0, i = 0;
         StudentData sd = new StudentData();
         NUMBER_OF_STUDENTS = Integer.parseInt(args[0]);
         NUMBER_OF_SUBJECTS = Integer.parseInt(args[1]);
         Course.readMe(i, NUMBER_OF_STUDENTS, NUMBER_OF_SUBJECTS);
+        Print.print(NUMBER_OF_STUDENTS, NUMBER_OF_SUBJECTS);
+        Highest.highest(NUMBER_OF_STUDENTS, NUMBER_OF_SUBJECTS);
+        StudentName.studentname(NUMBER_OF_STUDENTS, NUMBER_OF_SUBJECTS);
     }
 }
 final class Course {
-    private Course() {
+   Course() {
     }
-    private static Student[] allstudents;
+    static Student[] allstudents;
     public static Course readMe(int i, final int NUMBER_OF_STUDENTS, final int NUMBER_OF_SUBJECTS) {
         Course c = new Course();
         c.allstudents = new Student[NUMBER_OF_STUDENTS];
@@ -23,36 +26,6 @@ final class Course {
             System.out.println("enter the details for the student" + i);
             c.allstudents[i] = Student.readMe(NUMBER_OF_SUBJECTS);
             i++;
-        }
-        System.out.println("name     subject     marks");
-        for (int j = 0; j < NUMBER_OF_STUDENTS; j++) {
-            System.out.print(c.allstudents[j].name);
-            for (int k = 0; k < NUMBER_OF_SUBJECTS; k++) {
-                if (NUMBER_OF_SUBJECTS >= 1) {
-                    System.out.println("\t" + c.allstudents[j].allmarks[k].sub + "\t" + c.allstudents[j].allmarks[k].marks + "\t");
-                    // + c.allstudents[j].total1);
-                }
-            }
-        }
-        int highest = 0;
-        String name = "";
-        for (int p = 0; p < NUMBER_OF_SUBJECTS; p++) {
-            if (c.allstudents[p].total1 > highest) {
-                highest = c.allstudents[p].total1;
-                name = c.allstudents[p].name;
-            }
-        }
-        System.out.println("student who got high total marks is" + name);
-        String indname = " ";
-        for (int q = 0; q < NUMBER_OF_SUBJECTS; q++) {
-            int highestmarks = 0;
-            for (int r = 0; r < NUMBER_OF_STUDENTS; r++) {
-                if (c.allstudents[r].allmarks[q].marks > highestmarks) {
-                    highestmarks = c.allstudents[r].allmarks[q].marks;
-                    indname = c.allstudents[r].name;
-                }
-            }
-            System.out.println(indname + " got higest marks of " + highestmarks + "  in " + c.allstudents[q].allmarks[q].sub);
         }
         return c;
     }
@@ -89,4 +62,51 @@ class Marks {
         m.marks = sc.nextInt();
         return m;
     }
+}
+final class Print {
+      public static void print(int NUMBER_OF_STUDENTS,int NUMBER_OF_SUBJECTS) {
+      int j,k;
+Course c = new Course();
+   //System.out.println("name     subject     marks");
+           for (j = 0; j < NUMBER_OF_STUDENTS; j++) {
+            System.out.print(c.allstudents[j].name);
+            for (k = 0; k < NUMBER_OF_SUBJECTS; k++) 
+                if (NUMBER_OF_SUBJECTS >= 1) 
+                    System.out.println("\t" + c.allstudents[j].allmarks[k].sub + "\t" + c.allstudents[j].allmarks[k].marks + "\t");
+            }   
+             
+        }
+}
+final class Highest {
+        static int highest = 0;
+        static String name = "";
+    public static void highest(int NUMBER_OF_STUDENTS,int NUMBER_OF_SUBJECTS) {
+Course c = new Course();
+        for (int p = 0; p < NUMBER_OF_SUBJECTS; p++) {
+            if (c.allstudents[p].total1 > highest) {
+                highest = c.allstudents[p].total1;
+                name = c.allstudents[p].name;
+       System.out.println("student who got high total marks is" + name);
+        }
+    }
+  }
+}
+final class StudentName
+{
+       // System.out.println("student who got high total marks is" + name);
+        static String indname = " ";
+public static void studentname(int NUMBER_OF_STUDENTS,int NUMBER_OF_SUBJECTS)
+{
+        for (int q = 0; q < NUMBER_OF_SUBJECTS; q++) {
+            int highestmarks = 0;
+Course c = new Course();
+            for (int r = 0; r < NUMBER_OF_STUDENTS; r++) {
+                if (c.allstudents[r].allmarks[q].marks > highestmarks) {
+                    highestmarks = c.allstudents[r].allmarks[q].marks;
+                    indname = c.allstudents[r].name;
+                }
+            }
+            System.out.println(indname + " got higest marks of " + highestmarks + "  in " + c.allstudents[q].allmarks[q].sub);
+        }
+     }
 }
