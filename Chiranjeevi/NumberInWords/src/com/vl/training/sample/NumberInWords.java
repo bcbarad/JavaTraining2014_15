120e849c854be11d774fe44cdedcb94da5d6a53c
chiranjeevi <chiranjeevi.kollipara@valuelabs.net>
2014-11-28 10:16:50 +0530

    Updates Number To Words

--- Chiranjeevi/NumberInWords/src/com/vl/training/sample/NumberInWords.java
+++ Chiranjeevi/NumberInWords/src/com/vl/training/sample/NumberInWords.java
@@ -2,6 +2,7 @@ package com.vl.training.sample;
 
 public class NumberInWords {
     public static int pos;
+    public static String inWords="";
     public static void main(String[] arr) {
         if (arr.length == 1) {
             int n = Integer.parseInt(arr[0]);
@@ -9,8 +10,7 @@ public class NumberInWords {
                 System.out.println("Zero");
             }
             else if (n > 0 ) {
-                toWords(n);
-                System.out.println();
+                System.out.println(toWords(n));                
             }
             else {
                 System.out.println("Please enter a positive integer");
@@ -20,44 +20,44 @@ public class NumberInWords {
             System.out.println("Pass Exactly one number");
         }
     }
-    public static void toWords(int n) {
+    public static String toWords(int n) {
         String[] ones = {"","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
         String[] tens = {"","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty"};
-        String[] spl = {"","hundred","thousand","lakh","crore"};
-                
+        String[] spl = {"","hundred","thousand","lakh","crore"};        
         if(n < 20) {
-            System.out.print(ones[n % 20]+" ");
+            inWords = inWords + ones[n%20]+" ";
         }
         else if (n < 100) {
-            System.out.print(tens[n / 10]+" ");
+            inWords = inWords + tens[n / 10]+" ";
             toWords(n % 10);
         }
         else{
             pos++;
             if (n < 1000) {
                 toWords(n/100);
-                System.out.print(spl[pos % 5]+" ");
+                inWords = inWords + spl[pos % 5]+" ";
                 pos--;
                 if (n % 100 != 0)
-                    System.out.print("and ");
+                    inWords = inWords + "and ";
                 toWords(n % 100);
             }
             else {       
                 pos++;
                 if(n < 100000) {
                 toWords(n/1000);
-                System.out.print(spl[(pos % 5) ]+" ");
+                inWords = inWords + spl[(pos % 5) ]+" ";
 	        pos-=2 ;
                 toWords(n%1000);
                 }
                 else {
                     pos++;
                     toWords(n/100000);
-                    System.out.print(spl[(pos % 5)]+" ");
+                    inWords = inWords + spl[(pos % 5)]+" ";
                     pos-=3;
                     toWords(n %100000);
                 }
 	    }
         }
+        return inWords;
     }
 }
