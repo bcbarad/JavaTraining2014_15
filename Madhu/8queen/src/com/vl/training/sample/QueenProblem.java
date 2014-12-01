da6ec9cae3970b3e9410f80dc3492739f7bbc229
Madhusudhan123 <madhusudhana.swarna@valuelabs.net>
2014-12-01 13:00:01 +0530

    modified the 8queen code to print all solutions

--- Madhu/8queen/src/com/vl/training/sample/QueenProblem.java
+++ Madhu/8queen/src/com/vl/training/sample/QueenProblem.java
@@ -4,37 +4,42 @@ public final class QueenProblem {
     }
     static final int SIZE = 8;
     private static int[] board = new int[SIZE];
-    private static int row = 0 , column = 0;
     public static void main(final String[] args) {
-        boolean temp = QueenProblem.checkPosition(row , column);
-        if (temp) {                                                //checks no possible place on the row for queen
+        int row = 0, column = 0;
+        boolean endsolution = QueenProblem.checkPosition(row , column);
+        if (!endsolution) {                                                //checks no possible place on the row for queen
             row--;
-            column = board[row] + 1;
-            if (column >= SIZE) {
-                row--;
+            if (row < 0) { 
+               System.out.println("----------------End--------------");
+            } else { 
                 column = board[row] + 1;
+                if (column >= SIZE) {
+                    row--;
+                    column = board[row] + 1;
+                }
+                QueenProblem.checkPosition(row , column);
             }
-            QueenProblem.checkPosition(row , column);
-        }
-        QueenProblem.displayPositions();
+        } 
     }
     public static boolean checkPosition(int row , int column) {
-        boolean temp = true;
+        boolean endsolution = false;
         for (; column < SIZE; column++) {
             if (QueenProblem.isSafe(row  , column)) {         // place  the queen on board if issafe()  be true
                 board[row] = column;
                 if (row == (SIZE - 1)) {
-                    temp = false;
-                    return temp;
+                    endsolution = true;
+                    return endsolution;
                 } else {
-                    temp = checkPosition(row + 1 , 0);       // recursive call will move to check the next position
+                    endsolution =checkPosition(row + 1 , 0);       // recursive call will move to check the next position
                 }
             }
-            if (!temp) {
-                return temp;
+            if (endsolution) {
+               QueenProblem.displayPositions();
+               endsolution=false;
+               return endsolution;
             }
         }
-        return temp;
+        return endsolution;
     }
     public static boolean isSafe(int row , int column) {    //returns is the position is safe or not to place the queen
         for (int i = 0; i < row; i++) {
@@ -46,7 +51,8 @@ public final class QueenProblem {
     }
     public static void displayPositions() {
         for (int i = 0; i < 8; i++) {
-            System.out.println("(" + i + "," + board[i] + ")");
+            System.out.print("(" + i + "," + board[i] + ")");
         }
+        System.out.print("\n");
     }
 }
