a842710ecfb099b6ff7d0b1baeb2f4c8dc03ad64
chaitanya <chaitanya@A7080Q1D.corp.ads.valuelabs.net>
2014-12-03 15:43:24 +0530

    Updated

--- Chaitanya/reuse/src/com/vl/training/sample/Course.java
+++ Chaitanya/reuse/src/com/vl/training/sample/Course.java
@@ -1,27 +1,11 @@
-//package com.vl.training.sample;
+package com.vl.training.sample;
 import java.util.Scanner;
-public final class Course {
-    private static Course c;
-    private int noofsubjects, noofstudents;
-    private static Student[] student;
-    private static Scanner sc;
-    public static void main(final String[] args) {
-        int n;
-        c = new Course();
-        System.out.println("Enter No Of Students");
-        sc = new Scanner(System.in);
-        c.noofstudents = sc.nextInt();
-        System.out.println("Enter No oF Subjects");
-        c.noofsubjects = sc.nextInt();
-        //int[] temp = new int[c.noofstudents];
-        student = new Student[c.noofstudents];
-        for (int i = 0; i < c.noofstudents; i++) {
-            c.student[ i ] = Student.readme(new Scanner(System.in), c.noofsubjects);
-        }
-        getTotalHighest();
-        Course.getIndividualHighest();
-    }
-    static void getTotalHighest() {
+interface Max {
+    abstract void getTotalHighest(Course c);
+    abstract void getIndividualHighest(Course c);
+}
+class Maxi implements Max{
+    public void getTotalHighest(Course c) {
         int highmax = 0;
         String highname = "";
         for (int i = 0; i < c.noofstudents; i++) {
@@ -36,6 +20,61 @@ public final class Course {
         }
         System.out.println("high marks are\t" + highmax + "name\t" + highname);
     }
+    public void getIndividualHighest(Course c) {
+        System.out.println("Enter subject name that You want to get maximum");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int max = 0;
+        String name = "";
+        for (int i = 0; i < c.noofstudents; i++) {
+            for (int j = 0; j < c.noofsubjects; j++) {
+                if (str.equals(c.student[ i ].allmarks[j].subname)) {
+                    if (max < c.student[ i ].allmarks[ j ].marks) {
+                        max = c.student[ i ].allmarks[ j ].marks;
+                        name = c.student[ i ].sname;
+                    }
+                }
+            }
+        }
+        System.out.println("student name" + name + "marks are" + max);
+    }
+}
+public final class Course extends Maxi {
+    public static Course c;
+    public int noofsubjects, noofstudents;
+    public static Student[] student;
+    public static Scanner sc;
+    public static void main(final String[] args) {
+        int n;
+        c = new Course();
+        System.out.println("Enter No Of Students");
+        sc = new Scanner(System.in);
+        c.noofstudents = sc.nextInt();
+        System.out.println("Enter No oF Subjects");
+        c.noofsubjects = sc.nextInt();
+        //int[] temp = new int[c.noofstudents];
+        student = new Student[c.noofstudents];
+        for (int i = 0; i < c.noofstudents; i++) {
+            c.student[ i ] = Student.readme(new Scanner(System.in), c.noofsubjects);
+        }
+        c.getTotalHighest(c);
+        c.getIndividualHighest(c);
+    }
+    /*static void getTotalHighest() {
+        int highmax = 0;
+        String highname = "";
+        for (int i = 0; i < c.noofstudents; i++) {
+            int marks = 0;
+            for (int j = 0; j < c.noofsubjects; j++) {
+                marks = marks + c.student[ i ].total[j];
+                if (marks > highmax) {
+                    highmax = marks;
+                    highname = c.student[ i ].sname;
+                }
+            }
+        }
+        System.out.println("high marks are\t" + highmax + "name\t" + highname);
+    }*/
     static void getIndividualHighest() {
         System.out.println("Enter subject name that You want to get maximum");
         String str = sc.next();
