2aa15c232aa3a756ae4e5b5e1d49f8c7bc735fd3
MounikaMylaram <mounika.mylaram@valuelabs.net>
2014-12-05 23:18:47 +0530

    INdentation program done

---
+++ MounikaM/Indentation/src/com/vl/training/sample/Sum.java
@@ -0,0 +1,16 @@
+package com.vl.trainig.sample;
+import java.util.Scanner;
+
+public class Sum {
+           public static void main(String[] args)               {
+           int sum = 0;
+        Scanner sc = new Scanner(System.in);
+           int num1 = sc.nextInt();
+ while (num1 != 0) {
+                     sum = num1 + sum;
+                     num1 = num1 - 1;
+}
+}
+   }
+
+
