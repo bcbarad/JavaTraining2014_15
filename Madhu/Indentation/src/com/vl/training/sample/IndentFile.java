ff7da36a2a2049949cebbd884248ee8c47e7e125
Madhusudhan123 <madhusudhana.swarna@valuelabs.net>
2014-12-08 12:38:48 +0530

    checking comments for indentation done

--- Madhu/Indentation/src/com/vl/training/sample/IndentFile.java
+++ Madhu/Indentation/src/com/vl/training/sample/IndentFile.java
@@ -20,10 +20,12 @@ public class IndentFile {
             boolean checkspace = false;
             while ((c = input.read()) != -1) {
                 char ch = (char) c;
+                if (ch == '/') {
+                    ch = IndentFile.checkConditions(input , output , ch);
+                }
                 if (ch == '{') {
                     depth += 4;
                     output.write(ch);
-                    
                 } else if (ch == '}') {
                     depth -= 4;
                     for (int i = 0; i < depth; i++) {
@@ -57,6 +59,30 @@ public class IndentFile {
                 output.close();
             }
         }
+    }
+    public static  char checkConditions(FileReader input , FileWriter output , char ch) throws IOException {
+        output.write(ch);
+        ch=(char)input.read();
+        if(ch == '/') {
+            while(ch != '\n') {
+                output.write(ch);
+                ch = (char) input.read();
+            }
+        } 
+        else if( ch == '*') {
+            output.write(ch);
+            ch = (char) input.read();
+            char nextch=(char)input.read();
+            while(ch!='*' && nextch!='/') {
+                output.write(ch);
+                ch = nextch;
+                nextch = (char)input.read();
+            }
+            output.write(ch);
+            output.write(nextch);
+            ch = (char)input.read();
+        }
+        return ch;
     }
 }
 
