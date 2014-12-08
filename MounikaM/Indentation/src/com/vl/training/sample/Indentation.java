64fd0efdc7594081e22d50e7a1d3721c783b0207
MounikaMylaram <mounika.mylaram@valuelabs.net>
2014-12-08 23:59:06 +0530

    changes done to Indetation

--- MounikaM/Indentation/src/com/vl/training/sample/Indentation.java
+++ MounikaM/Indentation/src/com/vl/training/sample/Indentation.java
@@ -3,61 +3,117 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.FileWriter;
 
 
 public class Indentation {
-    public static void main(String[] args) throws IOException {
-        File file = null;
-        file = new File(args[0]);
-        Indentation in = new Indentation();
-        in.indentMethod(file);
+    public static void main(String[] args) {
+        File file1 = null;
+        File file2 = null;
+        try {
+            if (args.length == 2) {
+                file1 = new File(args[0]);
+                file2 = new File(args[1]);
+                Indentation in = new Indentation();
+                in.indentMethod(file1, file2);
+            } else {
+                System.out.println("\n enetr  only two files one is source file and another one is destination");
+            }
+        } catch (FileNotFoundException nfe) {
+            System.err.println(" Source file not present: Please give Corect file name");
+        } catch (IOException io) {
+            System.err.println(io.getMessage());
+        }
 
     }
-    public void indentMethod(File file) throws IOException {
+    public void indentMethod(File file1, File file2) throws IOException {
         FileReader reader = null;
+        FileWriter writer = null;
         try {
             int a = 0, depth = 0;
             char c;
-            boolean flag = false;
-
-            reader = new FileReader(file);
+            boolean isSpace = false;
+            boolean isBracesinSpecialSymbols = false;
+            reader = new FileReader(file1);
+            writer = new FileWriter(file2);
 
             while ((a = reader.read()) != -1) {
                 c = (char) a;
 
                 if (c == '{') {
-                    depth += 4;
-                    System.out.print(c);
-                } else if (c == '}') {
-                    depth = depth - 4;
-                    System.out.println();
-                    for (int i = 0; i < depth; i++) {
-                        System.out.print(" ");
+                    if (!isBracesinSpecialSymbols) {
+                        depth += 4;
+                        writer.write(c);
+                        isSpace = true;
+                    } else {
+                        writer.write(c);
+                        isSpace = false;
                     }
-                    System.out.print(c);
+                } else if (c == '}') {
+                    if (!isBracesinSpecialSymbols) {
+                        depth = depth - 4;
+                        writer.write('\n');
+                        printSpaces(depth, writer);
+                        writer.write(c);
+                        isSpace = true;
+                    } else {
+                        writer.write(c);
+                    }
                 } else if (c == ' ') {
-                    if (!flag) {
-                        System.out.print(" ");
+                    if (!isSpace) {
+                        writer.write(" ");
                     }
                 } else if (c == '\n') {
-                    System.out.print(c);
-                    flag = true;
-                } else {
-                    if (flag) {
-                        for (int i = 0; i < depth; i++) {
-                            System.out.print(" ");
-                        }
-                        flag = false;
+                    isSpace = true;
+                    isBracesinSpecialSymbols = false;
+                } else if (c == '"' || c == '\'') {
+                    writer.write(c);
+                    isBracesinSpecialSymbols = true;
+                } else if (c == '/') {
+                    writer.write(c);
+                    char ch = (char) reader.read();
+                    if (ch == '/') {
+                        writer.write(ch);
+                        isBracesinSpecialSymbols = true;
                     }
-                    System.out.print(c);
+                    if (ch == '*') {
+                        writer.write(ch);
+                        ch = (char) reader.read();
+                        char nextc = (char) reader.read();
+                        while (ch != '*' && nextc != '/') {
+                            ch = nextc;
+                            nextc = (char) reader.read();
+                            writer.write(ch);
+                        }
+                        writer.write(nextc);
+                    }
+                } else {
+                    if (isSpace) {
+                        writer.write('\n');
+                        printSpaces(depth, writer);
+                        isSpace = false;
+                    }
+                    writer.write(c);
                 }
             }
-        } catch(FileNotFoundException nfe) {
-            System.err.println("File not found in specified path: ");
+        } catch (FileNotFoundException nfe) {
+            nfe.printStackTrace();
+            throw nfe;
         } catch (IOException io) {
             io.printStackTrace();
+            throw io;
         } finally {
-            reader.close();
+            if (reader != null) {
+                reader.close();
+            }
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+    public void printSpaces(int depth, FileWriter writer)throws IOException {
+        for (int i = 0; i < depth; i++) {
+            writer.write(' ');
         }
     }
 }
