0160639c21cb8f4b6861caf4baa327e386532b9a
MounikaMylaram <mounika.mylaram@valuelabs.net>
2014-12-09 00:05:47 +0530

    changes done to Indentation Program

--- MounikaM/Indentation/src/com/vl/training/sample/Sum.java
+++ MounikaM/Indentation/src/com/vl/training/sample/Sum.java
@@ -5,8 +5,8 @@ public static void main(String[] args) { // { in main method defination
             String s = " { $ file}";
             char c = '{';
        int num1 = 0, sum = 0;
-       while(num1 != 10){  /* perfomin sum of fisrst 9 numbers
-                              code loop*/
+       while(num1 != 10){  /* performing sum of first 9 numbers using
+                              while loop*/
      sum = sum + num1;
        }
                }
