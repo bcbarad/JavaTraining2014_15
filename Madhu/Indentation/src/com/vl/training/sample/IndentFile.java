5e9f852c8bd3254fbf6b74345a943252684e0a72
Madhusudhan123 <madhusudhana.swarna@valuelabs.net>
2014-12-08 14:59:29 +0530

    modifications and conditions done on indent code

--- Madhu/Indentation/src/com/vl/training/sample/IndentFile.java
+++ Madhu/Indentation/src/com/vl/training/sample/IndentFile.java
@@ -7,9 +7,13 @@ public class IndentFile {
         try {
             FileReader input = null;
             FileWriter output = null;
-            input = new FileReader(args[0]);
-            output = new FileWriter(args[1]);
-            IndentFile.changeIndentation(input , output);
+            if(args.length < 1) {
+                System.out.println("Please make sure that pass an input text file");
+            } else {
+                input = new FileReader(args[0]);
+                output = new FileWriter(args[1]);
+                IndentFile.changeIndentation(input , output);
+            }
         } catch (IOException ie) {
             System.out.println(ie);
         }
@@ -20,7 +24,7 @@ public class IndentFile {
             boolean checkspace = false;
             while ((c = input.read()) != -1) {
                 char ch = (char) c;
-                if (ch == '/') {
+                if (ch == '/' || ch == '\'' || ch == '"') {
                     ch = IndentFile.checkConditions(input , output , ch);
                 }
                 if (ch == '{') {
@@ -84,6 +88,13 @@ public class IndentFile {
             output.write(ch);
             output.write(nextch);
             ch = (char)input.read();
+        } else {
+            while(ch != '\'' && ch!='"') {
+                output.write(ch);
+                ch=(char) input.read();
+             }
+             output.write(ch);
+             ch=(char) input.read();
         }
         return ch;
     }
