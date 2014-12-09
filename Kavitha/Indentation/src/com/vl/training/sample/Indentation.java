1b249ae427bad020b448685689a8132e14ee077e
kavitha <kavitha.bashagoni@valuelabs.net>
2014-12-09 13:50:35 +0530

    Added Indentation

--- Kavitha/Indentation/src/com/vl/training/sample/Indentation.java
+++ Kavitha/Indentation/src/com/vl/training/sample/Indentation.java
@@ -5,63 +5,93 @@ import java.io.IOException;
 import java.io.FileInputStream;
 
 public class Indentation {
-    public static void main(String []args) throws FileNotFoundException {
-        if(args.length > 0) {
+    public static void main(final String []args) throws FileNotFoundException {
+        if (args.length > 0) {
             File f = new File(args[0]);
-            Indentation.indent(f,args);
-        } 
-        else {
+            Indentation.indent(f, args);
+        } else {
             System.out.println("Please add the text file");
         }
-    }  
-        static void indent(File f,String []args) {  
-            if (!f.exists()) {
-                System.out.println(args[0] + "does not exists");
-                return;
-            }
-            if (!(f.isFile() && f.canRead())) {
-                System.out.println(f.getName() + "Can't be read");
-                return;
-            }
-            try {
-                FileInputStream fis = new FileInputStream(f);
-                char current = '\0';
-                int depth = 0,openCount = 0,closeCount = 0;
-                while (fis.available() > 0) {
+    }
+    static void indent(final File f, final String []args) {
+        if (!f.exists()) {
+            System.out.println(args[0] + "does not exists");
+            return;
+        }
+        if (!(f.isFile() && f.canRead())) {
+            System.out.println(f.getName() + "Can't be read");
+            return;
+        }
+        try {
+            FileInputStream fis = new FileInputStream(f);
+            char current, next, nextchar;
+            int depth = 0;
+            while (fis.available() > 0) {
+                current = (char) fis.read();
+                if (current == ' ') {
+                    System.out.print(current);
+                }
+                while (current == ' ') {
                     current = (char) fis.read();
-                    if (current== ' ') {
-                        System.out.print(current);
-                    }
-                    while (current == ' ') {
-                        current = (char) fis.read();
-                    }   
-                    if (current == '{') {
-                        System.out.print(current + "\n");
-                        depth += 4;
-                        openCount = depth;
-                        Indentation.displaySpace(openCount);
-                        current = (char) fis.read();
+                }
+                if (current == '{') {
+                    System.out.print(current + "\n");
+                    depth += 1;
+                    displaySpace(depth);
+                    current = (char) fis.read();
+                    continue;
+                } else if (current == '}') {
+                    depth -= 1;
+                    displaySpace(depth);
+                    System.out.print(current);
+                    continue;
+                } else if (current == ';') {
+                    System.out.print(current);
+                    displaySpace(depth);
+                    continue;
+                } else if (current == '/') {
+                    displaySpace(depth);
+                    System.out.print(current);
+                    next = (char) fis.read();
+                    if (next == '/') {
+                        System.out.print(next);
+                        while (next != '\n') {
+                            next = (char) fis.read();
+                            System.out.print(next);
+                        }
                         continue;
-                    } 
-                    if (current == '}') {
-                        depth -= 4;
-                        closeCount = depth;
-                        Indentation.displaySpace(closeCount);
-                        System.out.print(current);
-                        continue;
-                    } else {
-                      System.out.print(current);
                     }
+                    if (next == '*') {
+                        System.out.print(next);
+                        next = (char) fis.read();
+                        nextchar = (char) fis.read();
+                        while (next != '*' && nextchar != '/') {
+                            System.out.print(next);
+                            System.out.print(nextchar);
+                            if (nextchar == '\n') {
+                                displaySpace(depth);
+                            }
+                            next = (char) fis.read();
+                            nextchar = (char) fis.read();
+                        }
+                        System.out.print(next);
+                        System.out.print(nextchar);
+                        continue;
+                    }
+                } else {
+                    System.out.print(current);
                 }
             }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        static void displaySpace(int space) {
-            while(space>0) {
-                System.out.print(' ');
-                space--;
-            }
+    }
+    static void displaySpace(final int space) {
+        int i = 1;
+        while (i <= 4 * space) {
+            System.out.print(' ');
+            i++;
         }
+    }
 }
+
