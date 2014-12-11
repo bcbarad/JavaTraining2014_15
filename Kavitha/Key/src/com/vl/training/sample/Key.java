abdd2100a1375e543510db840e944bc2fd034ca4
kavitha <kavitha.bashagoni@valuelabs.net>
2014-12-11 16:33:43 +0530

    Added Key Program

--- Kavitha/Key/src/com/vl/training/sample/Key.java
+++ Kavitha/Key/src/com/vl/training/sample/Key.java
@@ -4,29 +4,29 @@ import java.util.*;
 public class Key {
     int n;
     String s;
-    Key(String s, int n) {
+    Key(final String s, final int n) {
         this.n = n;
         this.s = s;
     }
-    public static void main(String []args) {
-        Map<Key,String> m= new HashMap<Key,String>();
+    public static void main(final String []args) {
+        Map<Key, String> m = new HashMap<Key, String>();
         Key k1 = new Key("ABC", 100);
-        m.put(k1,"1000");
+        m.put(k1, "1000");
         k1.fun(m);
     }
-    void fun(Map m) {
-        Key k1 = new Key("ABC",100);
-        String result =(String) m.get(k1);
+    final void fun(Map m) {
+        Key k1 = new Key("ABC", 100);
+        String result = (String) m.get(k1);
         System.out.println(" " + result);
     }
-    public boolean equals(Object obj) {
-        if(this.s == s && this.n == n) {
+    public boolean equals(final Object obj) {
+        if (this.s == s && this.n == n) {
             return true;
-        }
-        else 
+        } else {
             return false;
+          }
     }
     public int hashCode() {
-        return 3*n + s.hashCode();
-     }
+        return 3 * n + s.hashCode();
+    }
 }
