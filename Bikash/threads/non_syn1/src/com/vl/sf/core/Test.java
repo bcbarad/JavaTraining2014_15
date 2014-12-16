338395a3d122bc28c0e5324e70003beafed7a647
SudAlmondSys <sudhakar.dharwada@valuelabs.com>
2014-12-16 13:17:39 +0530

    Added sleep in main method

--- Bikash/threads/non_syn1/src/com/vl/sf/core/Test.java
+++ Bikash/threads/non_syn1/src/com/vl/sf/core/Test.java
@@ -2,8 +2,15 @@ package com.vl.sf.core;
 public class Test{
     public static void main(String[] args){
         Add a=new Add();
+        System.out.println("Calling Thread 1");
         new Thread1(a).start();
+        System.out.println("Calling Thread 2");
         new Thread2(a).start();
+        try {
+            Thread.sleep(1000);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
