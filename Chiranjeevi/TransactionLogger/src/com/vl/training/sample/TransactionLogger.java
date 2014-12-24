1b3a9b272f3702cafe6fee5a3790ec4c9a6f6f98
chiranjeevi <chiranjeevi.kollipara@valuelabs.net>
2014-12-24 11:17:01 +0530

    Release of TransactionLogger v1.4

--- Chiranjeevi/TransactionLogger/src/com/vl/training/sample/TransactionLogger.java
+++ Chiranjeevi/TransactionLogger/src/com/vl/training/sample/TransactionLogger.java
@@ -2,28 +2,33 @@ package com.vl.training.sample;
 
 import java.util.Scanner;
 import java.util.Map;
+import java.util.concurrent.*;
 import java.util.HashMap;
+import java.util.Hashtable;
 
 class TransactionLogger extends Thread {
-    public synchronized Map processLog(final Scanner sc,
-            final HashMap<String, Long> log) {
+    public Map processLog(final Scanner sc,
+            final Hashtable<String, Long> log) {
         while (sc.hasNext()) {
+            System.out.println("Log1 : In thread " + Thread.currentThread().getId());
             String acc = sc.next();
             String remarks = sc.next();
-            long amount = sc.nextLong();
-            Long value = log.get(acc);
-            if (value == null) {
-                // do nothing
-            } else {
- //           synchronized(log) {
+            Long amount = sc.nextLong();
+            //synchronizing on the Hashtable
+            synchronized(log) {
+                Long value = log.get(acc);
+                if (value == null) {
+                    // do nothing
+                    value = amount;
+                } else {
                     if (remarks.equals("D")) {
-                        amount += value;
+                        value += amount;
                     } else if (remarks.equals("W")) {
-                        amount = value - amount;
+                        value -= amount;
                     }
-  //              }
+                }
+                log.put(acc, value);
             }
-            log.put(acc, amount);
         }
         return log;
     }
