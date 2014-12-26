571e05eebe154b49730c677af0854d5b0a937661
chiranjeevi <chiranjeevi.kollipara@valuelabs.net>
2014-12-26 17:49:16 +0530

    Release of TransactionLogger 1.5

--- Chiranjeevi/TransactionLogger/src/com/vl/training/sample/TransactionLogger.java
+++ Chiranjeevi/TransactionLogger/src/com/vl/training/sample/TransactionLogger.java
@@ -4,32 +4,65 @@ import java.util.Scanner;
 import java.util.Map;
 import java.util.concurrent.*;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Hashtable;
 
 class TransactionLogger extends Thread {
-    public Map processLog(final Scanner sc,
-            final Hashtable<String, Long> log) {
+
+    public void processLog(final Scanner sc,
+            final Hashtable<String, Account> log) {
         while (sc.hasNext()) {
-            System.out.print("Log1 : In thread " + Thread.currentThread().getId());
-            String acc = sc.next();
+            final String acc = sc.next();
             String remarks = sc.next();
             Long amount = sc.nextLong();
-            System.out.println(" Account : " + acc + "Amount : " + amount);
+            System.out.println("In thread: " + Thread.currentThread().getId() + " Account : " + acc + "Amount : " + amount);
             //synchronizing on the Hashtable
-            synchronized(acc) {
-                Long value = log.get(acc);
-                if (value == null) {
-                    value = amount;
-                } else {
-                    if (remarks.equals("D")) {
-                        value += amount;
-                    } else if (remarks.equals("W")) {
-                        value -= amount;
+            Account account = log.get(acc);
+                if (account == null) {
+                    if(remarks.equals("D")) {
+                        synchronized(log) {
+                            account = new Account(acc, amount);
+                            log.put(acc, account);
+                        }
                     }
+                } else {
+                    synchronized(account) {
+                        if (remarks.equals("D")) {
+                            account.deposit(amount);
+                        } else if (remarks.equals("W")) {
+                            account.withdraw(amount);
+                        }
+                        log.put(acc, account);
+                    }
+
                 }
-                log.put(acc, value);
-            }
         }
-        return log;
+    }
+}
+
+class Account {
+
+    String id;
+    long amount;
+
+    Account(String id, long amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    String getId() {
+        return this.id;
+    }
+
+    long getAmount() {
+        return this.amount;
+    }
+
+    void deposit(long amount) {
+        this.amount = this.amount + amount;
+    }
+
+    void withdraw(long amount) {
+        this.amount = this.amount - amount;
     }
 }
