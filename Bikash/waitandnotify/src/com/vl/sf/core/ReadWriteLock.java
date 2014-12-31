6c963d07c4ef9deb76d4a0925ce443d8046c390b
bcbarad <bikash.barad@valuelabs.net>
2014-12-31 11:01:03 +0530

    Delete ReadWriteLock.java

--- Bikash/waitandnotify/src/com/vl/sf/core/ReadWriteLock.java
+++
@@ -1,7 +0,0 @@
-package com.vl.sf.core;
-
-public interface ReadWriteLock {
-	public void getWriteLock()throws InterruptedException;
-	public void getReadLock()throws InterruptedException;
-	public void releaseLock();
-}
