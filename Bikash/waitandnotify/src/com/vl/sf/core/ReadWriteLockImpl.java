14be0b7d64f78e3c2171d96cebc36d3afd244762
bcbarad <bikash.barad@valuelabs.net>
2014-12-31 11:04:45 +0530

    Update ReadWriteLockImpl.java

--- Bikash/waitandnotify/src/com/vl/sf/core/ReadWriteLockImpl.java
+++ Bikash/waitandnotify/src/com/vl/sf/core/ReadWriteLockImpl.java
@@ -8,18 +8,6 @@ public class ReadWriteLockImpl {
 	public static synchronized void getReadLock() throws InterruptedException {
 		while (writers > 0 ) {
 			ReadWriteLockImpl.class.wait();
-	@Override
-	public void getReadLock() throws InterruptedException {
-		synchronized (object) {
-			if (!isWritting) {
-				System.out.println("U can read "
-						+ Thread.currentThread().getName());
-			} else {
-				System.out.println(Thread.currentThread().getName()+" waiting for writting ");
-				while(isWritting){
-					object.wait(100);
-				}
-			}
 		}
 		readers++;
 	}
@@ -33,18 +21,6 @@ public class ReadWriteLockImpl {
 
 		while (readers > 0 || writers > 0) {
 			ReadWriteLockImpl.class.wait();
-	@Override
-	public void releaseLock() {
-
-		synchronized (object) {
-			if (isWritting) {
-				System.out.println(Thread.currentThread().getName()+" read lock Realesed");
-				object.notifyAll();
-			} else {
-				System.out.println(Thread.currentThread().getName()+" write lock Realesed ");
-				isWritting = false;
-				object.notifyAll();
-			}
 		}
 		writers++;
 	}
