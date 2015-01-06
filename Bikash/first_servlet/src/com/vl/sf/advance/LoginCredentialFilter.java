37d5d99d3d4d890a07d3369602fca47e5939468b
bcbarad <bikash.barad@valuelabs.net>
2015-01-06 16:17:39 +0530

    Changes in Filter

--- Bikash/first_servlet/src/com/vl/sf/advance/LoginCredentialFilter.java
+++ Bikash/first_servlet/src/com/vl/sf/advance/LoginCredentialFilter.java
@@ -3,6 +3,7 @@ package com.vl.sf.advance;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Properties;
 
 import javax.servlet.Filter;
@@ -13,7 +14,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public class LoginCredentialFilter implements Filter {
-	Properties p = null;
+	Properties properties = null;
 
 	@Override
 	public void destroy() {
@@ -22,34 +23,29 @@ public class LoginCredentialFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain filterChain) throws IOException, ServletException {
-		File file = new File(
-				"/home/bikash/testprograms/Servlets/WebContent/WEB-INF/properties/logindetails.properties");
-		FileInputStream fis = new FileInputStream(file);
-
-		p = new Properties();
-
-		p.load(fis);
-		System.out.println("fis: " + fis);
+        
 		System.out.println("in filter");
-		String user=null;
-		String password=null;
-		int i=1;
-		boolean redirect=false;
-		while((user=p.getProperty("user"+i)) != null && (password=p.getProperty("password"+i))!=null){
-			if(user.equalsIgnoreCase(request.getParameter("user")) &&  password.equals(request.getParameter("password"))){
-				filterChain.doFilter(request, response);
-				redirect=true;
-			}
-			i++;
-		}
-		if(!redirect){
-			response.getWriter().print(" <font color=red>Invalid user and password</font>");
-			
+		String password = null;
+		if ((password = properties.getProperty(request.getParameter("user"))) != null
+				&& password.equals(request.getParameter("password"))) {
+			filterChain.doFilter(request, response);
+		} else {
+			response.getWriter().print(
+					" <font color=red>Invalid user and password</font>");
+
 		}
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
+	    try{
+	        properties = new Properties();
+            properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("filename.properties"));
+	    }catch(FileNotFoundException fne){
+	        fne.printStackTrace();
+	    }catch(IOException ioe){
+	        ioe.printStackTrace();
+	    }
 	}
 
 }
