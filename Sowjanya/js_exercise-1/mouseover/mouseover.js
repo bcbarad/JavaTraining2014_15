4275a2911f4bd2223afa44da20ced60068cdd348
sowjanyabatchu <sowjanya.batchu@valuelabs.net>
2015-01-22 12:21:46 +0530

    jsexerciseone completed

---
+++ Sowjanya/js_exercise-1/mouseover/mouseover.js
@@ -0,0 +1,8 @@
+function changeColorOnMouseOver(bg) {
+//just changes the background color when mouseover
+bg.style.backgroundColor = "red";
+}
+function changeColorOnMouseOut(bg) {
+//just changes the background color when mouseout
+bg.style.backgroundColor = "yellow";
+}
