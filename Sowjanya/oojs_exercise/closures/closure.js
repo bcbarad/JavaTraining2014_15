25f78d9bc0bfcf7882412116829637a093cf30ae
sowjanyabatchu <sowjanya.batchu@valuelabs.net>
2015-01-23 16:33:03 +0530

    closures added

---
+++ Sowjanya/oojs_exercise/closures/closure.js
@@ -0,0 +1,10 @@
+function showName (firstName, lastName){
+var name = "Your name is ";
+function makeFullName () {
+return (name + firstName + " " + lastName);
+	}
+return makeFullName();
+	}
+function myFunction() {
+    document.write(showName ("abcd", "efgh"));
+}
